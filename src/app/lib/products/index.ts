18763aac2fb23f982b51fd7d215475123f4f12aa
lihenko <obbdpu@gmail.com>
2026-07-16 09:46:42 +0300

    power bank kgp-598

--- src/app/lib/products/index.ts
+++ src/app/lib/products/index.ts
@@ -5,8 +5,10 @@ import { lenyesConfig } from './lenyes-px163';
 import { pressureWasherConfig } from './pressure-washer';
 import { soapDispenserConfig } from './soap-dispenser';
 import { electroturkaConfig } from './electro-turka';
+import { kingPowerKPG508Config } from './king-power-kpg-508';
 
 export const allProducts = [
+  kingPowerKPG508Config,
   electroturkaConfig,
   elektroMuhobijkaConfig,
   kinokiConfig,
